5a4bad5c62761539674f6a9aff340e6299f0a9d6
srinivasguntupalli <srinu.gnt2002@gmail.com>
2021-05-20 00:25:23 -0500

    Updated the app

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import joblib
 import numpy as np
 from prediction_service import prediction
 
-params_path = "params.yaml"
+
 webapp_root = "webapp"
 
 static_dir = os.path.join(webapp_root, "static")
